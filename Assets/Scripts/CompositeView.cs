63af1f481125d16ce10c323430367e816a4d837b
Simon But <firepillar2@gmail.com>
2021-10-27 00:10:28 +0800

    Using Shader Graph to generate different shapes

---
+++ Assets/Scripts/CompositeView.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompositeView : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject gridPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateTagGrid(new List<Vector2Int> { Vector2Int.zero,Vector2Int.up,Vector2Int.down,Vector2Int.left,Vector2Int.right,new Vector2Int(1,1) });
+        GenerateTagGrid(new List<Vector2Int> { new Vector2Int(-1, -1)});
+        GenerateTagGrid(new List<Vector2Int> { new Vector2Int(1, -1), new Vector2Int(1, -2) });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void GenerateTagGrid(List<Vector2Int> _gridContent)
+    {
+        GameObject _gameObjectInstance = new GameObject();
+        _gameObjectInstance.name = "tag";
+        _gameObjectInstance.transform.SetParent(canvas.transform);
+        _gameObjectInstance.transform.localPosition = new Vector2(0, 0);
+
+        //get content size
+        int size = 0;
+        foreach (Vector2 _v in _gridContent)
+        {
+            int compare = Mathf.FloorToInt(Mathf.Max(Mathf.Abs(_v.x), Mathf.Abs(_v.y)));
+            if (compare > size)
+            {
+                size = compare;
+            }
+        }
+
+        //generate content
+        for (int i = -size; i < size + 1; i++)
+        {
+            for (int j = -size; j < size + 1; j++)
+            {
+                if (_gridContent.Contains(new Vector2Int(i,j)))
+                {
+                    GameObject _gridInstance = Instantiate(gridPrefab);
+                    _gridInstance.transform.SetParent(_gameObjectInstance.transform);
+                    _gridInstance.transform.localPosition = new Vector2(i * 100f, j * 100f);
+
+                    Material mat = Instantiate(_gridInstance.GetComponent<Image>().material);
+                    //Material mat = new Material(Shader.Find("Shader Graphs/tag"));
+                    //mat.name = Random.Range(0, 10000).ToString();
+                    //mat.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+                    //generate edge and vertice
+                    if (!_gridContent.Contains(new Vector2Int(i - 1, j)))
+                    {
+                        mat.SetFloat("Left", 1);
+                        if (!_gridContent.Contains(new Vector2Int(i, j - 1)))
+                        {
+                            mat.SetFloat("LeftDownReverse", 1);
+                        }
+                        if (!_gridContent.Contains(new Vector2Int(i, j + 1)))
+                        {
+                            mat.SetFloat("LeftUpReverse", 1);
+                        }
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i, j - 1)))
+                    {
+                        mat.SetFloat("Down", 1);
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i - 1, j - 1)))
+                    {
+                        mat.SetFloat("LeftDown", 1);
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i + 1, j - 1)))
+                    {
+                        mat.SetFloat("RightDown", 1);
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i + 1, j)))
+                    {
+                        mat.SetFloat("Right", 1);
+                        if (!_gridContent.Contains(new Vector2Int(i, j - 1)))
+                        {
+                            mat.SetFloat("RightDownReverse", 1);
+                        }
+                        if (!_gridContent.Contains(new Vector2Int(i, j + 1)))
+                        {
+                            mat.SetFloat("RightUpReverse", 1);
+                        }
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i, j + 1)))
+                    {
+                        mat.SetFloat("Up", 1);
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i + 1, j + 1)))
+                    {
+                        mat.SetFloat("RightUp", 1);
+                    }
+                    if (!_gridContent.Contains(new Vector2Int(i - 1, j + 1)))
+                    {
+                        mat.SetFloat("LeftUp", 1);
+                    }
+
+                    _gridInstance.GetComponent<Image>().material = mat;
+                }
+            }
+        }
+
+    }
+}
