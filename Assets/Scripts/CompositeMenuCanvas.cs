f5eed71e28dc3c1f62c37d02b19c45c2708c9220
Simon But <firepillar2@gmail.com>
2021-12-08 00:57:26 +0800

    Add Composite Step 1

--- Assets/Scripts/CompositeMenuCanvas.cs
+++ Assets/Scripts/CompositeMenuCanvas.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ClassHelper;
+using UnityEngine.UI;
 
 public class CompositeMenuCanvas : ControlableUI
 {
@@ -31,15 +32,25 @@ public class CompositeMenuCanvas : ControlableUI
 
     void GenerateList()
     {
-        foreach (Asset _a in Database.userDataJson.assetList)
+        foreach (Asset _a in AssetManager.Instance.GetAssetListByType(recipe.assetTypeList[session]))
         {
             GameObject gridItemInstance = gridScrollView.GenerateItem("", _a.assetUid);//Graph TODO
+            //Graph if used
         }
     }
 
     void ClickData(int uid, GridItem gi)
     {
-
+        foreach (int _usedUid in assetSelectList)
+        {
+            if (_usedUid == uid)
+            {
+                print("this is already used");
+                return;
+            }
+        }
+        assetSelectList[session] = uid;
+        NextSession();
     }
 
     void SelectingData(int uid, GridItem gi)
@@ -65,5 +76,68 @@ public class CompositeMenuCanvas : ControlableUI
         //{
         //    OnBackPressed();
         //}
+    }
+
+    public List<GameObject> sessions;
+    public int[] assetSelectList;
+    RecipeData recipe;
+    public int session = 0;
+    public void AddUI(int _recipeId)
+    {
+        recipe = AssetManager.Instance.GetRecipeData(_recipeId);
+        assetSelectList = new int[recipe.assetTypeList.Count];
+        DefineSessions();
+        session = 0;
+        AddUI();
+    }
+
+    void DefineSessions()
+    {
+        List<int> atl = recipe.assetTypeList;
+        for (int i = 0; i < sessions.Count; i++)
+        {
+            if (i < atl.Count)
+            {
+                sessions[i].SetActive(true);
+                AssetTypeData _atd = AssetManager.Instance.GetAssetTypeData(atl[i]);
+                sessions[i].transform.Find("Text").GetComponent<Text>().text = _atd.name.GetString();
+            }
+            else
+            {
+                sessions[i].SetActive(false);
+            }
+        }
+    }
+
+    void NextSession()
+    {
+        bool isAllChossen = true;
+        int nextSession = 0;
+        for (int i = 0; i < assetSelectList.Length; i++)
+        {
+            int _s = (i + session) % assetSelectList.Length;
+            if (assetSelectList[_s] == 0)
+            {
+                isAllChossen = false;
+                nextSession = _s;
+                break;
+            }
+        }
+        if (isAllChossen)
+        {
+            print("NextStep");
+            return;
+        }
+        else
+        {
+            SelectSession(nextSession);
+        }
+    }
+
+    public void SelectSession(int _session)
+    {
+        print("select session " + _session);
+        session = _session;
+        Refresh();
     }
 }
